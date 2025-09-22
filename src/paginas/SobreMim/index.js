ebc0664f31e99f2472c6168d6219db2d4ef7ca94
unknown <miguel2002lamonier@gmail.com>
2025-09-22 11:57:17 -0300

    rotas aninhadas com react-router-dom

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,3 +1,34 @@
+import PostModelo from "componentes/PostModelo";
+import fotoCapa from "assets/sobre_mim_capa.png";
+import styles from "./SobreMim.module.css";
+import minhaFoto from "assets/minha_foto.jpeg";
+
 export default function SobreMim() {
-  return <h1>Sobre mim...</h1>;
+  return (
+    <PostModelo fotoCapa={fotoCapa} titulo={"Sobre mim"}>
+      <h3 className={styles.subtitulo}>Olá, eu sou o Miguel!</h3>
+      <img
+        src={minhaFoto}
+        alt="Foto do Miguel Lamonier sorrindo"
+        className={styles.fotoSobreMim}
+      />
+
+      <p className={styles.paragrafo}>
+        Profissional com mais de 7 anos de experiência em educação e tecnologia,
+        atuando como Instrutor de cursos profissionalizantes na área de TI.
+      </p>
+      <p className={styles.paragrafo}>
+        Tenho sólido conhecimento em informática, programação Front-End (HTML,
+        CSS, JavaScript, React) e grande facilidade de aprendizado. Sou
+        apaixonado por ensinar, aprender e enfrentar novos desafios, estando
+        sempre atualizado nas tendências tecnológicas.
+      </p>
+
+      <p className={styles.paragrafo}>
+        Busco novas oportunidades como Desenvolvedor Front-End Júnior, onde
+        posso aliar minha experiência didática ao desenvolvimento de soluções
+        inovadoras.
+      </p>
+    </PostModelo>
+  );
 }
